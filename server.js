2cca8959d0e4cdc9b6943453151690347eb4504e
Wiraszka1 <przybeckikrzysiek@gmail.com>
2023-09-28 18:18:13 +0200

    change of server.js file

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Grupa ZDTESTpol135 jest super SUPER!');
+  res.end(`<html><body><h1>This is HTML</h1></body></html>`);
 }
 
 const server = http.createServer(requestListener);
